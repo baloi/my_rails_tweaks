250d7ce40193976b17e2e481ba02134000404828
baloi <baloi.galia@gmail.com>
2011-06-04 22:14:19 -0400

    First commit

---
+++ test/integration/my_integration_test.rb
@@ -0,0 +1,26 @@
+require 'integration_test_helper'
+
+#class MyIntegrationTest < ActionDispatch::IntegrationTest
+#
+#  # Replace this with your real tests.
+#  test "viewing products" do
+#    visit '/products'
+#    save_and_open_page
+#    assert page.has_content?('Listing products')
+#  end
+#
+#  test "adding a club" do
+#     visit '/clubs'
+#     click_link "New Club"
+#     assert page.has_content?('Create a new club')
+#     fill_in 'Name', :with => 'Capybara'
+#     check 'Exclusive'
+#     click 'Create Club'
+#     assert page.has_content?('Club was successfully created.')
+#     click_link "Back"
+#     assert page.has_content?('Listing clubs')
+#     assert page.has_content?('Capybara true')
+#     assert page.has_no_content?('Capybara false')
+#  end
+#
+#end
